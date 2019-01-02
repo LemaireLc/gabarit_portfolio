40ccbab21ce36a5241cc51b1d5818f7eaed52cad
LemaireLc <lemairel@hotmail.fr>
2019-01-02 17:50:12 +0100

    Beta site 2.0

---
+++ index.php
@@ -0,0 +1,493 @@
+<!DOCTYPE html>
+<!--[if IE 9]> <html lang="en" class="ie9"> <![endif]-->
+<!--[if !IE]><!-->
+<html lang="fr">
+<!--<![endif]-->
+	<head>
+		<meta charset="utf-8">
+		<title>Bienvenu sur Mon Portfolio ;D</title>
+		<meta name="description" content="mon site">
+		<meta name="author" content="virak cool trip">
+
+		<!-- Mobile Meta -->
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+		<!-- Favicon -->
+		<link rel="shortcut icon" href="images/favicon.ico">
+
+		<!-- Web Fonts -->
+		<link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,400,700,300&amp;subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Raleway:700,400,300' rel='stylesheet' type='text/css'>
+
+
+		<!-- Bootstrap core CSS -->
+
+		<link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+
+		<!-- W3.CSS core CSS -->
+		<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+		<!-- Font Awesome CSS -->
+		<link href="fonts/font-awesome/css/font-awesome.css" rel="stylesheet">
+
+		<!-- Plugins -->
+		<link href="css/animations.css" rel="stylesheet">
+
+		<!-- Worthy core CSS file -->
+		<link href="css/style.css" rel="stylesheet">
+
+		<!-- Custom css -->
+		<link href="css/custom.css" rel="stylesheet">
+	</head>
+
+	<body class="no-trans">
+		<!-- scrollToTop -->
+		<!-- ================ -->
+		<div class="scrollToTop"><i class="icon-up-open-big"></i></div>
+
+		<!-- header start -->
+		<!-- ================ -->
+		<header class="header fixed clearfix navbar navbar-fixed-top">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-4">
+
+						<!-- header-left start -->
+						<!-- ================ -->
+						<div class="header-left clearfix">
+
+							<!-- logo -->
+							<div class="logo smooth-scroll">
+								<a href="#banner"><img id="logo" src="images/logo.gif" alt="logo"></a>
+							</div>
+						</div>
+						<!-- header-left end -->
+
+					</div>
+					<div class="col-md-8">
+
+						<!-- header-right start -->
+						<!-- ================ -->
+						<div class="header-right clearfix">
+
+							<!-- main-navigation start -->
+							<!-- ================ -->
+							<div class="main-navigation animated">
+
+								<!-- navbar start -->
+								<!-- ================ -->
+								<nav class="navbar navbar-default" role="navigation">
+									<div class="container-fluid">
+
+										<!-- Toggle get grouped for better mobile display -->
+										<div class="navbar-header">
+											<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-1">
+												<span class="sr-only">Toggle navigation</span>
+												<span class="icon-bar"></span>
+												<span class="icon-bar"></span>
+												<span class="icon-bar"></span>
+											</button>
+										</div>
+
+										<!-- Collect the nav links, forms, and other content for toggling -->
+										<div class="collapse navbar-collapse scrollspy smooth-scroll" id="navbar-collapse-1">
+											<ul class="nav navbar-nav navbar-right">
+												<li><a href="#about">Présentation</a></li>
+												<li><a href="#cv">Expériences</a></li>
+												<li><a href="#contact">Contact</a></li>
+											</ul>
+										</div>
+
+									</div>
+								</nav>
+								<!-- navbar end -->
+
+							</div>
+							<!-- main-navigation end -->
+
+						</div>
+						<!-- header-right end -->
+
+					</div>
+				</div>
+			</div>
+		</header>
+		<!-- header end -->
+
+		<!-- banner start -->
+		<!-- ================ -->
+		<div id="banner" class="banner">
+			<div class="banner-image"></div>
+			<div class="banner-caption">
+				<div class="container">
+					<div class="row">
+						<div class="space"></div>
+						<div class="col-md-8 col-md-offset-2 object-non-visible" data-animation-effect="fadeIn" >
+							<h1 class="text-center"><span>Loïc Lemaire<br></span></h1>
+
+								<h2 class="text-center ml5">
+	  <span class="text-center hidden-xs text-wrapper">
+	    <span class="line line1"></span>
+	    <span class="letters letters-left">Infographiste</span>
+	    <span class="letters ampersand">&</span>
+	    <span class="letters letters-right">Web Designer</span>
+	    <span class="line line2"></span>
+	  </span>
+	</h2>
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- banner end -->
+
+		<!-- section start -->
+		<!-- ================ -->
+		<div class="container object-visible">
+			<div class="space"></div>
+			<br>
+			<br>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<h1 id="about" class="title text-center"><span>A propos de moi</span></h1>
+						<p class="lead text-center">Loïc Lemaire - Infografiste / Web Designer</p>
+						<div class="space"></div>
+						<div class="row">
+							<div class="col-md-6">
+								<img src="images/profil.gif" alt="">
+
+								<div class="space"></div>
+							</div>
+
+							<div class="col-md-6">
+                                <h3 class="text-left"><span>Présentation</span></h3>
+								<p>De formation scientifique, j'ai toujours eu une attirance pour l'informatique et les nouvelles technologies. Ayant des facilités dans l'autodidacte, je me suis formé par le biais d'internet à la maîtrise des différents outils de design et de programmation.
+								<p>De se fait je me s'emcibilisé sur différent aspect telque le responsive design et UI, UX design ainsi que sûr les types de référencements où d'optimisation du code afin d'avoir un site sécurisé et rapide.</p>
+							</div>
+						</div>
+						<div class="space"></div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- section end -->
+
+		<!-- section start -->
+		<!-- ================ -->
+		<div class="section translucent-bg bg-image-1 blue">
+			<div class="container object-visible">
+				<h1 id="cv"  class="text-center title">Mon Parcours</h1>
+				<img src="images/moi2.jpg" id="img" class="center-block" alt="">
+				<div class="space"></div>
+				<div class="row">
+					<div class="col-sm-6">
+						<div class="media">
+							<div class="media-body text-right">
+								<h4 class="media-heading">2018</h4>
+								<h4 class="media-heading"><span>SOCOFROID</span></h4>
+								<p>Graphiste freelance<br><span> - Remise à neuf du logo afin de mettre à jour les différents supports de communication.</span></p>
+							</div>
+              <div class="media-right">
+								<i class="fa fa-globe hidden-xs"></i>
+							</div>
+						</div>
+						<div class="media">
+							<div class="media-body text-right">
+								<h4 class="media-heading">2018</h4>
+								<h4 class="media-heading"><span>TOTAL</span></h4>
+								<p>Graphiste freelance<br><span> - Création d'image IMO (  soit à peu près 200 images ), pour facilités la création ainsi que la visibilité des plans de site.</span></p>
+							</div>
+							<div class="media-right">
+								<i class="fa fa-globe hidden-xs"></i>
+							</div>
+						</div>
+						<div class="media">
+							<div class="media-body text-right">
+								<h4 class="media-heading">2018</h4>
+								<h4 class="media-heading"><span>PÉTRO SERVICE</span></h4>
+								<p>Graphiste freelance<br><span> - Remise à neuf du logo. <br>- Création d'un autocollant descriptif des
+  consignes de sécurité dans les véhicules.</span></p>
+							</div>
+							<div class="media-right">
+								<i class="fa fa-globe hidden-xs"></i>
+							</div>
+						</div>
+					</div>
+					<div class="space visible-xs"></div>
+					<div class="col-sm-6">
+						<div class="media">
+								<div class="media-left">
+									<i class="fa fa-globe hidden-xs"></i>
+								</div>
+								<div class="media-body text-left">
+								<h4 class="media-heading">2018</h4>
+								<h4 class="media-heading"><span>CONNEXION GRAPHIQUE</span></h4>
+								<p>Formation<br><span> - Étude des principaux outils de l’infographie
+  ( Photoshop, Illustrator, InDesign ).</span></p>
+							</div>
+						</div>
+						<div class="media">
+							<div class="media-left">
+								<i class="fa fa-globe hidden-xs"></i>
+							</div>
+							<div class="media-body text-left">
+								<h4 class="media-heading">2017</h4>
+								<h4 class="media-heading"><span>BOUARA & CIE</span></h4>
+								<p>Graphiste freelance<br><span> - Création des identités visuelles telles que
+  le logo, la page d'en-tête, les cartes de visite,
+  ainsi que plusieurs autres étiquettes de tous type
+  adapté au différent support utiliser.</span></p>
+							</div>
+						</div>
+						<div class="media">
+							<div class="media-left">
+								<i class="fa fa-globe hidden-xs"></i>
+							</div>
+							<div class="media-body text-left">
+								<h4 class="media-heading">2015</h4>
+								<h4 class="media-heading"><span>IT PARIS EIFFEL</span></h4>
+								<p>Formation<br><span> - Étude des différents langage de programmation
+  ( HTML, CSS, PHP ).</span></p>
+							</div>
+						</div>
+					</div>
+				</div>
+<div class="space"></div>
+<div class="row">
+	<div class="col-md-6 hidden-xs">
+			<span id="sl_play" class="sl_command">&nbsp;</span>
+			<span id="sl_pause" class="sl_command">&nbsp;</span>
+
+
+
+			<section id="slideshow">
+
+				<a class="play_commands pause" href="#sl_pause" title="Maintain paused">Pause</a>
+				<a class="play_commands play" href="#sl_play" title="Play the animation">Play</a>
+
+				<div class="container">
+					<div class="c_slider"></div>
+					<div class="slider">
+						<figure>
+							<img src="images/exo-batman.png" alt=""/>
+							<figcaption>Exercice Connexion Graphique : Exercice d'applicaton des compétences</figcaption>
+						</figure><!--
+						--><figure>
+							<img src="images/GraphRev.png" alt=""/>
+							<figcaption>Exercice Connexion Graphique : Exercice d'applicaton des compétences</figcaption>
+						</figure><!--
+						--><figure>
+							<img src="images/avatar_Full_30129.jpg" alt=""/>
+							<figcaption>Exercice Connexion Graphique : Applicaton des Brush</figcaption>
+						</figure><!--
+						--><figure>
+							<img src="images/titre-RVB.png" alt=""/>
+							<figcaption>Exercice Connexion Graphique : Appliction des masques de fusion</figcaption>
+						</figure>
+					</div>
+				</div>
+
+				<span id="timeline"></span>
+
+			</section>
+			             <div class="space"></div>
+			        </div>
+							<div class="col-md-6">
+								<p>Afin de vous faire une idée de mes compétences, voici quelque exercices et projets personnel que j'ai réalisé aux cour de mon apprentissage.</p>
+								<p></p>
+								<a href="Book/index.html"> <input type="submit" value="Mon Book" class="btn btn-default"></a>
+
+							</div>
+						</div>
+			</div>
+		</div>
+
+		<!-- section end -->
+
+
+		<!-- footer start -->
+		<!-- ================ -->
+		<footer id="footer">
+
+			<!-- .footer start -->
+			<!-- ================ -->
+			<div class="footer section">
+				<div class="container">
+					<h1 class="title text-center" id="contact"><span>Contactez-moi</span></h1>
+					<div class="space"></div>
+					<div class="row">
+						<div class="col-sm-6">
+							<div class="footer-content">
+								<p class="large"> En espérent que se petit tour vous auras plus, n'ésité pas à me contacter pour toute information supplémentaire.</p>
+								<p class="large">A très bientôt!</p>
+								<ul class="list-icons">
+									<li><i class="fa fa-map-marker pr-10"></i>176 Impasse Guy de Maupassant 34070 Montpellier</li>
+									<li><i class="fa fa-phone pr-10"></i> +33 6 76 46 90 36</li>
+									<li><i class="fa fa-envelope-o pr-10"></i>lemairel@hotmail.fr</li>
+								</ul>
+								<ul class="social-links">
+
+									<li class="linkedin"><a target="_blank" href="http://www.linkedin.com/in/loïc-lemaire-a133ba83"><i class="fa fa-linkedin"></i></a></li>
+
+								</ul>
+							</div>
+						</div>
+						<?php
+						define( 'MAIL_TO', /* >>>>> */'lemairel@hotmail.fr'/* <<<<< */ );  //ajouter votre courriel
+						    define( 'MAIL_FROM', 'Votre E-mail' ); // valeur par défaut
+						    define( 'MAIL_OBJECT', 'objet du message' ); // valeur par défaut
+						    define( 'MAIL_MESSAGE', 'votre message' ); // valeur par défaut
+
+						    $mailSent = false; // drapeau qui aiguille l'affichage du formulaire OU du récapitulatif
+						    $errors = array(); // tableau des erreurs de saisie
+
+						    if( filter_has_var( INPUT_POST, 'send' ) ) // le formulaire a été soumis avec le bouton [Envoyer]
+						    {
+						        $from = filter_input( INPUT_POST, 'from', FILTER_VALIDATE_EMAIL );
+						        if( $from === NULL || $from === MAIL_FROM ) // si le courriel fourni est vide OU égale à la valeur par défaut
+						        {
+						            $errors[] = 'Vous devez renseigner votre adresse de courrier électronique.';
+						        }
+						        elseif( $from === false ) // si le courriel fourni n'est pas valide
+						        {
+						            $errors[] = 'L\'adresse de courrier électronique n\'est pas valide.';
+						            $from = filter_input( INPUT_POST, 'from', FILTER_SANITIZE_EMAIL );
+						        }
+
+						        $object = filter_input( INPUT_POST, 'object', FILTER_SANITIZE_STRING, FILTER_FLAG_ENCODE_HIGH | FILTER_FLAG_ENCODE_LOW );
+						        if( $object === NULL OR $object === false OR empty( $object ) OR $object === MAIL_OBJECT ) // si l'objet fourni est vide, invalide ou égale à la valeur par défaut
+						        {
+						            $errors[] = 'Vous devez renseigner l\'objet.';
+						        }
+
+						 /* pas besoin de nettoyer le message.
+						 / [http://www.phpsecure.info/v2/article/MailHeadersInject.php]
+						 / Logiquement, les parties message, To: et Subject: pourraient servir aussi à injecter quelque chose,  mais la fonction mail()
+						 / filtre bien les deux dernières, et la première est le message, et à partir du moment où on a sauté une ligne dans l'envoi du mail,
+						 / c'est considéré comme du texte; le message ne saurait donc rester qu'un message.*/
+						        $message = filter_input( INPUT_POST, 'message', FILTER_UNSAFE_RAW );
+						        if( $message === NULL OR $message === false OR empty( $message ) OR $message === MAIL_MESSAGE ) // si le message fourni est vide ou égale à la valeur par défaut
+						        {
+						            $errors[] = 'Vous devez écrire un message.';
+						        }
+
+						        if( count( $errors ) === 0 ) // si il n'y a pas d'erreurs
+						        {
+						            if( mail( MAIL_TO, $object, $message, "From: $from\nReply-to: $from\n" ) ) // tentative d'envoi du message
+						            {
+						                $mailSent = true;
+						            }
+						            else // échec de l'envoi
+						            {
+						                $errors[] = 'Votre message n\'a pas été envoyé.';
+						            }
+						        }
+						    }
+						    else // le formulaire est affiché pour la première fois, avec les valeurs par défaut
+						    {
+						        $from = MAIL_FROM;
+						        $object = MAIL_OBJECT;
+						        $message = MAIL_MESSAGE;
+						    }
+						?>
+						<?php
+						    if( $mailSent === true ) // si le message a bien été envoyé, on affiche le récapitulatif
+						    {
+						?>
+						        <p id="success">Votre message a bien été envoyé.</p>
+						        <p><strong>Courriel pour la réponse :</strong><br /><?php echo( $from ); ?></p>
+						        <p><strong>Objet :</strong><br /><?php echo( $object ); ?></p>
+						        <p><strong>Message :</strong><br /><?php echo( nl2br( htmlspecialchars( $message ) ) ); ?></p>
+						<?php
+						    }
+						    else // le formulaire est affiché pour la première fois ou le formulaire a été soumis mais contenait des erreurs
+						    {
+						        if( count( $errors ) !== 0 )
+						        {
+						            echo( "\t\t<ul>\n" );
+						            foreach( $errors as $error )
+						            {
+						                echo( "\t\t\t<li>$error</li>\n" );
+						            }
+						            echo( "\t\t</ul>\n" );
+						        }
+						        else
+						        {
+						            echo( "\t\t<p id=\"welcome\"><em>Tous les champs sont obligatoires</em></p>\n" );
+						        }
+						?>
+						<div class="col-sm-6">
+							<div class="footer-content">
+								<form role="form" id="footer-form contact" method="post" action="<?php echo( $_SERVER['REQUEST_URI'] ); ?>">
+									<div class="form-group has-feedback">
+										<label class="sr-only" for="from">E-mail address</label>
+										<input type="email" class="form-control" id="from" placeholder="Votre e-mail" name="from" required>
+										<i class="fa fa-user form-control-feedback"></i>
+									</div>
+									<div class="form-group has-feedback">
+										<label class="sr-only" for="object">Object du message</label>
+										<input type="text" class="form-control" id="object" placeholder="Object du message" name="object" required>
+										<i class="fa fa-envelope form-control-feedback"></i>
+									</div>
+									<div class="form-group has-feedback">
+										<label class="sr-only" for="message">Message</label>
+										<textarea class="form-control" rows="8" id="message" placeholder="Laissez votre message" name="message" required></textarea>
+										<i class="fa fa-pencil form-control-feedback"></i>
+									</div>
+									<input type="submit" value="Envoyez" name="send" class="btn btn-default">
+								</form>
+							</div>
+						</div>
+						<?php
+						    }
+						?>
+					</div>
+				</div>
+			</div>
+			<!-- .footer end -->
+
+			<!-- .subfooter start -->
+			<!-- ================ -->
+			<div class="subfooter">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12">
+							<p class="text-center">Copyright © 2018 Portfolio Loïc Lemaire.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- .subfooter end -->
+
+		</footer>
+		<!-- footer end -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js"></script>
+
+		<!-- JavaScript files placed at the end of the document so the pages load faster
+		================================================== -->
+		<!-- Jquery and Bootstap core js files -->
+		<script type="text/javascript" src="plugins/jquery.min.js"></script>
+		<script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+
+		<!-- Modernizr javascript -->
+		<script type="text/javascript" src="plugins/modernizr.js"></script>
+
+		<!-- Isotope javascript -->
+		<script type="text/javascript" src="plugins/isotope/isotope.pkgd.min.js"></script>
+
+		<!-- Backstretch javascript -->
+		<script type="text/javascript" src="plugins/jquery.backstretch.min.js"></script>
+
+		<!-- Appear javascript -->
+		<script type="text/javascript" src="plugins/jquery.appear.js"></script>
+
+		<!-- Initialization of Plugins -->
+		<script type="text/javascript" src="js/template.js"></script>
+
+		<!-- Custom Scripts -->
+		<script type="text/javascript" src="js/custom.js"></script>
+
+	</body>
+</html>
